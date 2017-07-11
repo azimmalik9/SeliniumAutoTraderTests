1338f42a4c2ac2bfea795307e30145b9c0672f16
azimmalik9 <azim.malik9@gmail.com>
2017-07-11 12:02:37 +0100

    Add files via upload

---
+++ website/src/test/java/autotrader/website/Tests/AppTest.java
@@ -0,0 +1,122 @@
+package autotrader.website.Tests;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+import autotrader.website.App;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+   // extends TestCase
+{
+	ExtentReports report;
+	ExtentTest test;
+	WebDriver driver;
+	App homePage;
+	
+	@BeforeClass()
+	public void setup(){   
+		report = new ExtentReports(
+				"C:\\Users\\Azim\\Desktop\\Selinium2\\automationreport.html",true);
+		test = report.startTest("Autotrader tests");
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Azim\\Desktop\\Selinium2\\chromedriver.exe");
+		
+		driver = new ChromeDriver();
+		
+		driver.get("http://www.autotrader.co.uk/");
+
+		homePage = new App(driver);
+		test.log(LogStatus.INFO, "Browser started");
+
+	}
+	@Test(priority = 1, enabled = true)
+	public void testTitle() {
+		String homePageTitle = homePage.getTitle();
+		if (homePageTitle.contains("Auto Trader UK | Find New & Used Cars for Sale")) {
+			test.log(LogStatus.PASS, "Verify home page title");
+		} else {
+			test.log(LogStatus.FAIL, "Verify home page title");
+		}
+	}
+	@Test(priority = 1, enabled = true)
+	public void testCarSearchRadius() {
+		homePage.selectRadius();
+		String radiusText = homePage.getRadius();
+		if (radiusText.contains("Within 100 miles")) {
+			test.log(LogStatus.PASS, "verify radius input");
+		} else {
+			test.log(LogStatus.FAIL, "verify radius input");
+		}
+	}
+	@Test(priority = 3, enabled = true)
+	public void testCarSearchMake() {
+		try{
+			homePage.selectMake();
+			String makeText = homePage.getMake();
+			if (makeText.contains("Ford")) {
+				test.log(LogStatus.PASS, "verify make input");
+			} else {
+				test.log(LogStatus.FAIL, "verify make input");
+			}
+		}catch(NoSuchElementException x){
+			System.out.println("That make of car might not exist");
+		}
+	}
+	@Test(priority = 4, enabled = true)
+	public void testCarSearchModel() {
+		try{
+			homePage.selectModel();
+			String modelText = homePage.getModel();
+			if (modelText.contains("Focus")) {
+				test.log(LogStatus.PASS, "verify model input");
+			} else {
+				test.log(LogStatus.FAIL, "verify model input");
+			}
+		}catch(NoSuchElementException x){
+			System.out.println("That model of car might not exist");
+		}
+	}
+	@Test(priority = 5, enabled = true)
+	public void testCarSearchMaxPrice() {
+		homePage.selectMaxPrice();
+		String priceText = homePage.getMaxPrice();
+		if (priceText.contains("To £100,000")) {
+			test.log(LogStatus.PASS, "verify max price input");
+		} else {
+			test.log(LogStatus.FAIL, "verify max price input");
+		}
+	}
+	@Test(priority = 6, enabled = true)
+	public void testCarPostcode() {
+		homePage.getPostcodeInput("CR4 1AA");
+		String postcodeInputText = homePage.getPostcodeTextBoxText();
+		if (postcodeInputText.equals("CR4 1AA")) {
+			test.log(LogStatus.PASS, "Verify postcode input");
+		} else {
+			test.log(LogStatus.FAIL, "Verify postcode input");
+		}
+		homePage.clickSearch();
+		
+		test.log(LogStatus.INFO, "Browser closed");
+		report.endTest(test);
+		report.flush();
+		tearDown();
+
+	}
+	public void tearDown() {
+		try {
+			driver.close();
+		} catch (Exception ex) {
+			System.out.println(ex.toString());
+		}
+	}
+
+}
